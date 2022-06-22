ec370896f72ba488192e9ca9ac6a9d1cf1b27d0f
Kaiki <dalvamso60@gmail.com>
2022-06-21 22:12:16 -0300

    Exercicio 02

---
+++ modulo_inicial/exercicio_2.php
@@ -0,0 +1,13 @@
+<?php
+
+    echo "Exercicio 2\n";
+
+    echo "informe um numero : \n";
+
+    $numero = readline();
+
+    if($numero > 1 and $numero < 9)
+    echo "O valor esta na faixa permitida";
+    else
+    echo "O valor ultrapassou a faixa";
+?>
