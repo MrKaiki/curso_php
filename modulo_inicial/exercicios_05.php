d9b63cbe234b5e22bb2fcecd62570c3a736d7b43
Kaiki <dalvamso60@gmail.com>
2022-06-23 19:44:52 -0300

    Exercicio 05

--- modulo_inicial/exercicios_05.php
+++ modulo_inicial/exercicios_05.php
@@ -2,21 +2,21 @@
 
   echo "Exercicio 05\n";
 
-  echo "Digite sua altura";
+  echo "Digite sua altura:> ";
   $altura = readline();
 
-  echo "Digite seu sexo";
+  echo "Digite seu sexo:> ";
   $sexo = readline();
 
-  $alturaxpeso = 0.0;
+  $alturapeso = 0.0;
 
 
   if($sexo == "Masculino"){
-    $alturaxpeso += ((72.7*$altura)-58);
-    echo "Seu peso ideal é $alturaxpeso";
+    $alturapeso += 72.7*$altura-58;
+    echo "Seu peso ideal é $alturapeso";
   } elseif($sexo == "Feminino"){
-    $alturaxpeso = ((62.1*$altura)-44.7);
-    echo "Seu peso ideal é $alturaxpeso";
+    $alturapeso = 62.1*$altura-44.7;
+    echo "Seu peso ideal é $alturapeso";
   } 
 
 ?>
