9b479c95997a0be19fcec04295fa577b50ce9dc4
Kaiki <dalvamso60@gmail.com>
2022-06-23 19:10:37 -0300

    Exercicio 3

---
+++ modulo_inicial/exercicios_3.php
@@ -0,0 +1,22 @@
+<?php 
+
+    echo "Exercicio 2\n";
+
+    $sexo = readline();
+    $nome = readline();
+
+    echo "Digite seu sexo:> ";
+    echo "Digite seu nome:> ";
+
+    if($sexo = "Masculino"){
+        echo "Ilmo sr.";
+    } elseif($sexo = "Feminino"){
+        echo "Ilma sr.";
+    } 
+
+    echo "seu nome é @nome";
+    
+
+    
+
+?>
