84ad0ab361c96a23864a946a8d72000329778650
Kaiki <dalvamso60@gmail.com>
2022-06-23 22:04:40 -0300

    Exercicio 10 - PhP

---
+++ modulo_inicial/exercicios_10.php
@@ -0,0 +1,48 @@
+<?php 
+
+    echo "Exercicio 1\n";
+
+    echo "Digite um numero inteiro:> ";
+    $n1 = readline();
+
+    echo "Digite um numero inteiro:> ";
+    $n2 = readline();
+
+    echo "Digite um numero inteiro:> ";
+    $n3 = readline();
+
+    echo "Digite um numero inteiro:> ";
+    $n4 = readline();
+
+    echo "Digite um numero inteiro:> ";
+    $n5 = readline();
+
+
+    $media = ($n1 + $n2 + $n3 + $n4 + $n5) / 5;
+
+    if($n1 > $media){
+        echo "$n1 \n";
+    }
+
+    if($n2 > $media){
+        echo "$n2 \n";
+    }
+
+    if($n3 > $media){
+        echo "$n3 \n";
+    }
+
+    if($n4 > $media){
+        echo "$n4 \n";
+    }
+
+    if($n5 > $media){
+        echo "$n5 \n";
+    }
+
+
+
+
+
+
+?>
